8946d1c754dc8143347ad4c267f29a82aebf10a8
Chris McArthur <prince.chrismc@gmail.com>
2021-12-19 19:19:16 -0800

    migrate JS to TS

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,42 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
+
+// Permission levels higher in the array have higher access to the repo.
+const perms: readonly string[] = ['none', 'read', 'write', 'admin']
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const githubToken = core.getInput('github_token', { required: true })
+    const octokit = github.getOctokit(githubToken)
+    const username: Readonly<string> = github.context.actor
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const response = await octokit.rest.repos.getCollaboratorPermissionLevel({
+      ...github.context.repo,
+      username
+    })
 
-    core.setOutput('time', new Date().toTimeString())
+    const permission = response.data.permission // Permission level of actual user
+    const argPerm = core.getInput('permission', { required: true }) // Permission level passed in through args
+
+    const yourPermIdx = perms.indexOf(permission)
+    const requiredPermIdx = perms.indexOf(argPerm)
+
+    core.debug(`[Action] User Permission: ${permission}`)
+    core.debug(`[Action] Minimum Action Permission: ${argPerm}`)
+
+    // If the index of your permission is at least or greater than the required,
+    // exit successfully.
+    if (yourPermIdx < requiredPermIdx) {
+      core.setFailed(
+        `🚨 Insuffient Permissions! ${username} does not have ${argPerm} permissions`
+      )
+      core.setOutput('permitted', 'false')
+      return
+    }
+    core.info(`✔️ ${username} is permitted`)
+    core.setOutput('permitted', 'true')
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) core.setFailed(error)
   }
 }
 
